a645a4f4fe26cc7f060e42074a72b0c7348fdd7c
TuanLe30122003 <91524625+TuanLe30122003@users.noreply.github.com>
2022-09-22 07:22:35 +0700

    movement (2)

---
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -0,0 +1,34 @@
+package uet.oop.bomberman.entities;
+
+
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.event.EventHandler;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.*;
+
+public class Bomber extends Entity {
+
+    public Bomber(int x, int y, Image img) {
+        super( x, y, img);
+    }
+
+    public  boolean isAlive = true;
+
+    public int directionX = 1;
+
+    public int directionY = 1;
+//    nhap su kien ban phim -> thay doi huong di cua bomber
+
+
+    @Override
+    public void update() {
+
+        this.x += directionX;
+        this.y += directionY;
+    }
+}
